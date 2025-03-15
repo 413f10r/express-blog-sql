6980f336704b44a23de5651b72652e374098138f
413f10r <ale-fioretti@live.it>
2025-03-15 01:01:40 +0100

    add: .env

--- data/db.js
+++ data/db.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2")
 
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "db-posts"
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME,
+
 });
 
 connection.connect((err) => {
